48d857dcc5618fd699806465fb6da46df49c6acd
ASUS <rewxrld@gmail.com>
2026-06-09 12:05:09 +0300

    logg smpt

--- app/mail/smtp_client.py
+++ app/mail/smtp_client.py
@@ -1,4 +1,5 @@
 from email.mime.text import MIMEText
+from pydoc import text
 import aiosmtplib
 
 from app.config import *
@@ -10,6 +11,11 @@ async def send_reply(to_email, subject, text):
     msg["From"] = YANDEX_EMAIL
     msg["To"] = to_email
     msg["Subject"] = f"Re: {subject}"
+
+    print("Sending email...")
+    print("TO:", repr(to_email))
+    print("SUBJECT:", repr(subject))
+    print("TEXT:", repr(text))
 
     await aiosmtplib.send(
         msg,
